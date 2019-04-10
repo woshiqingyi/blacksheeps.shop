e11a916200d051d84a87df7f8e82de597fcc7a0c
woshiqingyi <1355224991@qq.com>
2019-04-10 17:59:34 +0800

    1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="woshiqingyi">woshiqingyi</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+
+<style>
+.woshiqingyi{
+  height: 100px;
+ 
+  background-color: black
+}
+</style>
+
