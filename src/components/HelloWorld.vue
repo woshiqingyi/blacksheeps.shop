72a2cfa280cf30b01dfcfb3999e68f8a86cbac73
woshiqingyi <1355224991@qq.com>
2019-04-15 22:35:59 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,47 @@
 <template>
-   <div class="woshiqingyi">woshiqingyi</div>
+  <div class="layout">
+   <div class="woshiqingyi">
+     <div class="" v-if="show">iph</div>
+     <div class="" v-if="show">ipa</div>
+     <div class="" v-if="show">Mac</div>
+     <div class="" v-if="show">Watch</div>
+     <div class="" >Music</div>
+   </div>
+  </div>
 </template>
-
 <script>
 export default {
-  
+  data: function () {
+    return {
+    }
+  },
+  beforeCreate () {
+    if (navigator.userAgent.match(/(iPhone|iPod|Android|ios|iPad)/i)) {
+      this.show = true
+    } else {
+      this.show = false
+    }
+  },
+  methods: {
+  }
 }
 </script>
-
-
 <style>
 .woshiqingyi{
-  height: 100px;
- 
-  background-color: black
+  height: 50px;
+  color:white;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-left: 200px;
+  margin-right: 200px;
 }
-</style>
 
+.layout{
+  margin:0px;
+  background-color: black;
+ }
+  body{
+      margin:0px;
+   }
+ </style>
