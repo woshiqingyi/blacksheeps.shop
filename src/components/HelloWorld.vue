3fcd650d5b5a4e24dcdbb3ead6daba27c963255f
woshiqingyi <1355224991@qq.com>
2019-04-15 23:57:40 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,10 @@
      <div class="" v-if="show">Mac</div>
      <div class="" v-if="show">Watch</div>
      <div class="" >Music</div>
+     <select v-model="selected" @change='getValue'>
+       <option>--请选择--</option>
+       <option v-for="item in optList" v-bind:key='item'>{{item}}</option>
+     </select>
    </div>
   </div>
 </template>
@@ -13,20 +17,30 @@
 export default {
   data: function () {
     return {
+      selected: '',
+      optList: ['青龙', '白虎', '朱雀', '玄武']
     }
   },
   beforeCreate () {
     if (navigator.userAgent.match(/(iPhone|iPod|Android|ios|iPad)/i)) {
-      this.show = true
-    } else {
       this.show = false
+      console.log('1')
+    } else {
+      this.show = true
+      console.log('4')
     }
   },
   methods: {
+    getValue () {
+      console.log('您选择了', this.selected)
+    }
   }
 }
 </script>
 <style>
+.nishi{
+  color: white;
+}
 .woshiqingyi{
   height: 50px;
   color:white;
