871447654d600091db8d72c6e4f1cbc876795345
woshiqingyi <1355224991@qq.com>
2019-08-12 16:26:46 +0800

    1

---
+++ src/components/commoditydetails.vue
@@ -0,0 +1,79 @@
+<template>  
+    <div class="layout">
+      <div class="per-commodity"></div>
+      <div class="per-commodity"></div>
+      <div class="per-commodity"></div>
+    </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      // bannerImg: require('../image/clothes.png')
+    }
+  },
+  beforeCreate () {
+    var dWidth = document.documentElement.clientWidth
+    if (dWidth > 500) dWidth = 500
+    var baseFontSize = dWidth * 100 / 750
+    document.documentElement.style.fontSize = baseFontSize + 'px'
+    if (navigator.userAgent.match(/(iPhone|iPod|Android|ios|iPad)/i)) {
+    } else {
+    }
+  },
+
+  created () {
+    var dWidth = document.documentElement.clientWidth
+    var shezhi = this.CommodityCategory.length * 3 
+    if (dWidth > 500) dWidth = 500
+    var baseFontSize = dWidth * 100 / 750
+    var jieguo = shezhi * baseFontSize
+    console.log('222',shezhi)
+    console.log('333',baseFontSize)
+    console.log('444',jieguo)
+    console.log('555',dWidth)
+    console.log('666',document.documentElement.clientWidth)
+    if (jieguo > document.documentElement.clientWidth) {
+      this.show = true
+    }
+  },
+
+  mounted () {
+    window.onresize = () => {
+      return (() => {
+        var dWidth = document.documentElement.clientWidth
+        if (dWidth > 500) dWidth = 500
+        var baseFontSize = dWidth * 100 / 750
+        document.documentElement.style.fontSize = baseFontSize + 'px'
+      })()
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped>
+ 
+  .layout{
+    display: flex;
+    flex-wrap: wrap;
+    padding:5%;
+  }
+  /* 换行，当元素大于父元素后，就自动换行 */
+  .per-commodity{
+    height: 700px;
+    width: 35%; 
+    background-color: whitesmoke;
+   
+    
+  }
+  .layout1{
+    display:flex;
+    justify-content: center;
+  }
+  div{
+   
+  }
+ </style>
---
+++ src/components/homepage/introduce.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="introduce_layout">
+    <div class="introduce_style">
+      <div class="mini_navigation"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.introduce_layout {
+  display: flex;
+  justify-content: center;
+}
+
+.introduce_style {
+  /* border: 1px gray solid;
+  height: 700px; */
+  width: 1300px;
+  margin-top: 20px;
+}
+.mini_navigation {
+  width: 100%;
+  height: 800px;
+  background-color: whitesmoke;
+  /* border: 1px rgb(212, 212, 212) solid; */
+}
+</style>
